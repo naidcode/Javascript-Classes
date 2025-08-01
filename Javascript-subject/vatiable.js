cb2c31c43fd07ccde0ccafb6e77aa39aa3271084
naidcode <naidcode.dev@gmail.com>
2025-08-02 00:38:44 +0530

    Javascript-Subjects

---
+++ Javascript-subject/vatiable.js
@@ -0,0 +1,14 @@
+let variable = 10;
+console.log(variable);
+
+variable = 20;
+console.log(variable); //we can write variables like this also
+
+variable = variable + 10;
+console.log(variable);
+
+let result = variable + 10; //we can calculate using variables
+console.log(result);
+
+let name = "nahid pasha ";
+console.log(name, variable);
