0bb3cb08b0888e39ad3fc1b1f0974fc2cfe4d293
naidcode <naidcode.dev@gmail.com>
2025-08-09 00:40:34 +0530

    all-exer-1

---
+++ all-exercise.js
@@ -0,0 +1,64 @@
+// let student = {
+//   name: 'nahid pasha',
+//   age: 20,
+//   isstudent: true
+// }
+
+// console.log(student);
+
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log(num1 + num2)
+
+// let marks = 85;
+
+// if (marks >= 90) {
+//   console.log('A grade')
+// } else if (marks >= 75) {
+//   console.log('B grade')
+// } else if (marks >= 50) {
+//   console.log('C grade ')
+// } else {
+//   console.log('Fail')
+// }
+
+
+// let name = 'nahid';
+// let age = 20;
+// let height = 170;
+
+// console.log(`hi my name is ${name} and my age is ${age} and my height is ${height} cm`)
+
+// let num1 = 10;
+// let num2 = 5;
+
+// let sum = num1 + num2;
+// let division = num1 / num2;
+// let sub = num1 - num2;
+// let multi = num1 * num2 ;
+// let percentage = num1 % num2;
+
+// console.log(`sum: ${sum}`)
+// console.log(`sum: ${division}`)
+// console.log(`sum: ${sub}`)
+// console.log(`sum: ${multi}`)
+// console.log(`sum: ${percentage}`)
+
+// let age = 15;
+
+// if (age >= 0 && age < 12){
+//   console.log('Child')
+// } else if (age >= 13 && age < 19) {
+//   console.log("Teenager")
+// } else if (age >= 20 && age < 59) {
+//   console.log("Adult")
+// } else if (age > 60) {
+//   console.log('Senior')
+// } else {
+//   console.log("Senior citizen")
+// }
+
+
+
+
