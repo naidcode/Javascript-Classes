1b01276711a246c53b2b44e19bf457f8e500fcf8
naidcode <naidcode.dev@gmail.com>
2025-08-16 19:27:08 +0530

    advanced-function

---
+++ Javascript-subject/Advanced-functions.js
@@ -0,0 +1,20 @@
+// setTimeout(() => {
+//   console.log("fazilpasha")
+//   console.log("nahid pasha")  /*this function work after how time you set i set 3000. 
+//                                 // 3000 mean 3s after 3s the code will run */
+// }, 3000);
+
+// console.log("Next line")  //this line will run first because he will never wait 3s for run.
+
+
+[
+"dishes",
+'wash',
+"night"
+].forEach( function name(params) {
+  
+})
+
+let nahid = "";
+
+nahid.forEach()
