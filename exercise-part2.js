db6b05dd19e1be616f37f4ac38793457aeefb159
naidcode <naidcode.dev@gmail.com>
2025-08-22 18:00:37 +0530

    exercise-part2-newupdate

--- exercise-part2.js
+++ exercise-part2.js
@@ -1,41 +1,54 @@
-function test() {
-  if (true) {
-    var x = 10;
-    var y = 20;
-    var z = 30;
-  }
-  console.log(x); // ?
-  console.log(y); // ?
-  console.log(z); // ?
-}
-test();
-var a = 5;
-var b = 5;
-console.log(a);  
+// function test() {
+//   if (true) {
+//     var x = 10;
+//     var y = 20;
+//     var z = 30;
+//   }
+//   console.log(x); // ?
+//   console.log(y); // ?
+//   console.log(z); // ?
+// }
+// test();
+// var a = 5;
+// var b = 5;
+// console.log(a);  
 
 
-console.log(b); 
+// console.log(b); 
 
-sayHi();  
+// sayHi();  
 
-function sayHi() {
-  console.log("Hello!");
-}
+// function sayHi() {
+//   console.log("Hello!");
+// }
 
-console.log(sayHello)
+// console.log(sayHello)
 
-var sayHello = function() {
-  return "Hi again!";
-};
+// var sayHello = function() {
+//   return "Hi again!";
+// };
 
 
-function example() {
- // ?
-  let num = 10;
-    console.log(num);
-}
+// function example() {
+//  // ?
+//   let num = 10;
+//     console.log(num);
+// }
  
-example();
+// example();
+
+nameHi()
+function nameHi() {
+   var a = 11;
+  console.log(a)
+}
+
+nameHi();
+
+var nam  = function () {
+  console.log("hello")
+}
+nam()
 
 
 
