0a32e1627b5b8abd490cf202c3543ca7bc38ef50
naidcode <naidcode.dev@gmail.com>
2025-08-22 16:12:12 +0530

    exercise -part2

---
+++ exercise-part2.js
@@ -0,0 +1,42 @@
+function test() {
+  if (true) {
+    var x = 10;
+    var y = 20;
+    var z = 30;
+  }
+  console.log(x); // ?
+  console.log(y); // ?
+  console.log(z); // ?
+}
+test();
+var a = 5;
+var b = 5;
+console.log(a);  
+
+
+console.log(b); 
+
+sayHi();  
+
+function sayHi() {
+  console.log("Hello!");
+}
+
+console.log(sayHello)
+
+var sayHello = function() {
+  return "Hi again!";
+};
+
+
+function example() {
+ // ?
+  let num = 10;
+    console.log(num);
+}
+ 
+example();
+
+
+
+
