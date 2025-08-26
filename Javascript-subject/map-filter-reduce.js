4fc2ce2dc66482b5fcd719049b98385a2c872674
naidcode <naidcode.dev@gmail.com>
2025-08-26 09:22:47 +0530

    rename

--- Javascript-subject/map-filter-reduce.js
+++ Javascript-subject/map-filter-reduce.js
@@ -41,7 +41,7 @@
 
 let number = [1, 2, 3, 45, 6, 34, 35, 35];
 let number2 = [11, 12, 13, 14, 15, 16, 17];
-let number3 = [1, 2, 3, 4, 56, 7, 9,10,20,30,40];
+let number3 = [1, 2, 3, 4, 56, 7, 9, 10, 20, 30, 40];
 let letter = ["b", "a", "d", "c"];
 
 let a = number.toString(); //make array element into string
@@ -50,14 +50,14 @@ console.log(a);
 let b = number.join("-"); // join anything between the array element
 console.log(b);
 
-number.push(20); //push return new element in array
+number.push(20); //push return new element in array  at last
 console.log(number);
 
-let c = number.shift(); //shift remove first element from array
+let c = number.shift(); //shift remove first element from array  from start
 console.log(c);
 console.log(number);
 
-let e = number.unshift(100); //add element inthe starting of the array
+let e = number.unshift(100); //add element inthe starting of the array  from start
 console.log(e);
 console.log(number);
 
@@ -73,14 +73,25 @@ console.log(g);
 let h = number2.reverse(); //reverse the element of the array.
 console.log(h);
 
-  //splice we can use to add new element inthe array.
-let i = number3.splice(1,3,100, "added");
-console.log(number3)
+//splice we can use to add new element in the array.
+let i = number3.splice(1, 3, 100, "added");
+console.log(number3);
 console.log(i);
 
-let j = number3.slice(3,6);  //slice out a piece from an array and create a new array.
+let j = number3.slice(3, 6 ); //slice out a piece from an array and create a new array.
 console.log(j);
 
-// let z = number.pop();
-// console.log(number);
-// console.log(z); //remove last element from the array
+let obj =[ 
+{ name: "fazil", id: 1 },
+{ name: "nahid", id: 2 },
+{ name: "nahid", id: 3},
+{ name: "nahid", id: 4 },
+{ name: "nahid", id: 5 },]
+
+let k = obj.filter((item) => item.name == "nahid");
+console.log(k[0].name),
+ console.log(k.length);
+
+let z = number.pop();
+console.log(number);
+console.log(z); //remove last element from the array
