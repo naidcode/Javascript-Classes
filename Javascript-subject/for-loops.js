5c02debb290511a19abfd6597a451753e72df406
naidcode <naidcode.dev@gmail.com>
2025-08-26 16:04:55 +0530

    for-loops

---
+++ Javascript-subject/for-loops.js
@@ -0,0 +1,37 @@
+for (let index = 0; index < 20; index++) {
+  //For loop
+  console.log(index);
+}
+
+let obj = {
+  nahid: 90,
+  fazil: 85,
+  fouziya: 80,
+};
+for (const a in obj) {
+  if (Object.prototype.hasOwnProperty.call(obj, a)) {  //for in loop
+    const element = obj[a];   
+    console.log(`student marks ${a} and  ${element}`);
+  }
+}
+
+for (const b of "nahid") {
+  console.log(b.toLocaleUpperCase())   //for of loops
+}
+
+
+
+let n = 11
+let i = 1;
+while (i < n) {    //while loop
+console.log(i)
+i++
+}
+
+do {
+  i++
+} while (i < n);
+console.log(i)
+console.log(i)
+
+
