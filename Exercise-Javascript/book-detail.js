26a9ccbd99a0e5d6544bb66524cafbd0a79b6c80
naidcode <naidcode.dev@gmail.com>
2025-08-27 22:41:32 +0530

    format

--- Exercise-Javascript/book-detail.js
+++ Exercise-Javascript/book-detail.js
@@ -1,24 +1,25 @@
 let book = [];
 
-function addBooks(title , author , pages) {
+function addBooks(title, author, pages) {
   book.push({ title, author, pages });
 }
 
 function viewBook() {
-  book.forEach((book , index) => {
-     console.log(`${index + 1} - ${book.title} - ${book.author} - ${book.pages}`)
-  })
+  book.forEach((book, index) => {
+    console.log(
+      `${index + 1} - ${book.title} - ${book.author} - ${book.pages}`
+    );
+  });
 }
 
 function findBook(title) {
-return book.find( (book) => book.title === title )
+  return book.find((book) => book.title === title);
 }
 
 function checkLongBook() {
-  let longBook =  book.some((book) => book.pages > 300);
-  console.log(longBook)
+  let longBook = book.some((book) => book.pages > 300);
+  console.log(longBook);
 }
-
 
 function checkSmallBook() {
   let smallBook = book.every((book) => book.pages < 150);
@@ -26,20 +27,19 @@ function checkSmallBook() {
 }
 
 function sortBook() {
-  let sort = book.sort((book , book2) => book.pages - book2.pages);
+  let sort = book.sort((book, book2) => book.pages - book2.pages);
   console.log(sort);
 }
 
 function totalPagesBook() {
-  let total = book.reduce((acc , total) =>  acc + total.pages , 0  );
+  let total = book.reduce((acc, total) => acc + total.pages, 0);
   console.log(total);
 }
 
-
-addBooks("harry potter" , "rk", 300)
-addBooks("atomic habbit" , "nahid", 350)
-addBooks("48 laws" , "fazil", 120)
-addBooks("clean code" , "chat gpt", 180)
+addBooks("harry potter", "rk", 300);
+addBooks("atomic habbit", "nahid", 350);
+addBooks("48 laws", "fazil", 120);
+addBooks("clean code", "chat gpt", 180);
 
 viewBook();
 console.log(findBook("harry potter"));
