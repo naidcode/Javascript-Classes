84acad99dfb546a27546cea93a8a39748f14802e
naidcode <naidcode.dev@gmail.com>
2025-08-27 14:09:04 +0530

    runner

--- Javascript-subject/tempCodeRunnerFile.js
+++ Javascript-subject/tempCodeRunnerFile.js
@@ -1,10 +1,4 @@
-setTimeout(() => {
-//   console.log("fazilpasha")
-//   console.log("nahid pasha")  /*this function work after how time you set i set 3000. 
-//                                 // 3000 mean 3s after 3s the code will run */
-// }, 3000);
-
-// console.log("Next line")  //this line will run first because he will never wait 3s for run.
-
-
-
+for (let i = 0; i < dot.length; i++) {
+//   const element = dot[i];
+ 
+// }
