5cb7ee3767514b82e13217c5bb5fee25ab08c739
naidcode <naidcode.dev@gmail.com>
2025-08-31 16:18:13 +0530

    constructor

--- Exercise-Javascript/classes-objects.js
+++ Exercise-Javascript/classes-objects.js
@@ -1,28 +1,25 @@
 class attandence {
+  constructor(studentName , rollNo) {
+    console.log(this.name + this.roll)
+    this.name = studentName;
+    this.roll = rollNo;
+  }
   present() {
     console.log(this.name + "present" + " and his roll no is " + this.roll);
   }
   absent() {
     console.log(this.name + "absent" + " and his roll no is " + this.roll);
   }
-  fill(studentName , rollNo) {
-    this.name = studentName;
-    this.roll = rollNo;
-  }
-
   
+
 }
 
-let nahid = new attandence();
-nahid.fill("nahid is ", 10);
-let shahid = new attandence();
-shahid.fill("shahid is ", 12);
-let sahil = new attandence();
-sahil.fill("sahil is ", 35);
-let fazil = new attandence();
-fazil.fill("fazil is ", 13);
-
+let nahid = new attandence("nahid is ", 10)
+let shahid = new attandence("shahid is ", 12);
+let sahil = new attandence("sahil is ", 35);
+let fazil = new attandence("fazil is ", 13);
 nahid.absent();
 shahid.present();
 sahil.absent();
 fazil.present();
+
