2d8a3b7576d16eec596238d58aaff0eeff85d1ba
naidcode <naidcode.dev@gmail.com>
2025-08-31 15:47:51 +0530

    spread

--- Javascript-subject/object.js
+++ Javascript-subject/object.js
@@ -17,11 +17,11 @@
 
 // console.log(product.price);  //not having property if we use result show undefined.
 
-// product.prices = "2500";  // if you want to add new property use this. 
+// product.prices = "2500";  // if you want to add new property use this.
 // console.log(product)
 
 // delete product.prices;  //to delete the property from object use this.
-// console.log(product) 
+// console.log(product)
 
 // let product2 = {
 //   name: "fazil pasha ",
@@ -42,12 +42,10 @@
 // console.log(product2.rating.stars);
 // product2.fun();
 
-
 // console.log(JSON.stringify(product2));
 
 // const jsonstring = JSON.stringify(product2);
 // console.log(JSON.parse(jsonstring));
-
 
 // console.log('hello'.length);
 // console.log('hello'.toUpperCase())
@@ -55,20 +53,20 @@
 // ------- EXAMPLE 1-------
 // SWAP 2 ELEMENTS IN AN ARRAYS
 
-let colors = ["red" , "green" , "blue", "white" , "black"];
-[colors[1] ,colors[2]] = [colors[2] ,colors[1]];
+let colors = ["red", "green", "blue", "white", "black"];
+[colors[1], colors[2]] = [colors[2], colors[1]];
 
 console.log(colors);
 
 // ------- EXAMPLE 2 -------
 // ASSAIN ARRAY ELEMENT TO VARIABLES
 
-let [a,b,c , ...d] = colors;
+let [a, b, c, ...d] = colors;
 
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
 
 //------- EXAMPLE 3 ------
 // EXTRACT VALUES FROM OBJECTS
@@ -77,64 +75,61 @@ let person1 = {
   firstName: "Nahid",
   lastName: "pasha",
   age: 20,
-  
-}
+};
 let person2 = {
   firstName: "fazil",
   lastName: "pasha",
   age: 23,
-  job: "sales man"
+  job: "sales man",
 };
 
-let {firstName , lastName , age , job } = person2;
+let { firstName, lastName, age, job } = person2;
 
-console.log(firstName)
-console.log(lastName)
-console.log(age)
-console.log(job)
-
-
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(job);
 
 //------- EXAMPLE 4 ------
 // DESTRUCTURE FUNCTION PARAMETERS
-function displayPerson({firstName , lastName, age ,job = "unemploed"}) {
-  console.log(`name: ${firstName} ${lastName}`)
-    console.log(`age: ${age}`)
-      console.log(`job: ${job}`)
-
+function displayPerson({ firstName, lastName, age, job = "unemploed" }) {
+  console.log(`name: ${firstName} ${lastName}`);
+  console.log(`age: ${age}`);
+  console.log(`job: ${job}`);
 }
-
 
 let person3 = {
   firstName: "Nahid",
   lastName: "pasha",
   age: 20,
-  
-}
+};
 let person4 = {
   firstName: "fazil",
   lastName: "pasha",
   age: 23,
-  job: "sales man"
+  job: "sales man",
 };
-
 
 displayPerson(person4);
 
-
 // SPREAD OPERATOR
 
-let numbers = [1,2,3,4,5,6];
-let min = Math.min(...numbers)
-let max = Math.max(...numbers)
+let numbers = [1, 2, 3, 4, 5, 6];
+let min = Math.min(...numbers);
+let max = Math.max(...numbers);
 
-console.log(min)
+console.log(min);
 console.log(max);
 
-let names = ["nahid" , "fazil" ,"shahid" , "sahil"];
-let lastname = ["pasha" , "khan" , "syed" , "md"];
+let names = ["nahid", "fazil", "shahid", "sahil"];
+let lastname = ["pasha", "khan", "syed", "md"];
 
-let everything = [...names , ...lastname , "fouzi" , "nasreen"];
+let everything = [...names, ...lastname, "fouzi", "nasreen"];
 
 console.log(everything);
 
+let name = "nahid";
+let agee = 20;
+
+let person = { name, age, class: "first year degree" };
+console.log(person);
