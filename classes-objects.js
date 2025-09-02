46d247f6446dbc3eea525b9db4adb77006b61a75
naidcode <naidcode.dev@gmail.com>
2025-09-03 00:39:34 +0530

    remove get and set

--- classes-objects.js
+++ classes-objects.js
@@ -62,33 +62,6 @@ console.log(MathHelper.add(5,2)); // 7
 
 
 
-//getter and setter
-
-class student{
-  constructor(name){
-    this.name = name
-  }
-
-  presents(){
-        console.log(`i'm a student`);
-
-  }
-
-  get name(){
-  return  this._name 
-  }
-
-  set name(newNmae){
-    this._name = newNmae
-  }
-
-};
-
-let stu  = new student("nahid");
-stu.presents();
-console.log(stu.name);
-stu.name = "fazil";
-console.log(stu.name);
 
 
 
