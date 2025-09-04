ce7a6ae15692b123b6803404ce56f44511989474
naidcode <naidcode.dev@gmail.com>
2025-09-04 06:44:27 +0530

    static

--- Javascript-subject/static-method.js
+++ Javascript-subject/static-method.js
@@ -19,3 +19,36 @@ console.log(MathHelper.pi);      // 3.14159
 console.log(MathHelper.add(5,2)); // 7
 console.log(MathHelper.sub(5,7))  // 8.85841
 console.log(MathHelper.divided(10,12,5)) // 10.183101550488765
+
+
+class User{
+  static usercount = 0
+  constructor(username){
+    this.username = username,
+    User.usercount++;
+  }
+
+
+ usernames(){
+  console.log(`hello my name is ${this.username}`)
+}
+
+static usercounts(){
+  console.log(`there are only ${User.usercount} users are there`)
+}
+
+}
+
+let user1 = new User("nahid");
+let user2 = new User("fazil")
+
+console.log(user1.username);
+console.log(user2.username)
+console.log(User.usercount)
+user1.usernames()
+user2.usernames()
+User.usercounts();
+
+
+
+
