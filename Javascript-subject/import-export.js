9b824c91785b82c25c7a28145cb627995b9f6329
naidcode <naidcode.dev@gmail.com>
2025-09-05 01:06:00 +0530

    import-export

---
+++ Javascript-subject/import-export.js
@@ -0,0 +1,9 @@
+import { viewStudent } from '../Exercise-Javascript/exercise-part2.js';
+import { sortProducts , Math} from '../Exercise-Javascript/shopping-cart-systum.js';
+let Ab = sortProducts();
+let Cd = viewStudent();
+let ef = Math(20,20);
+
+console.log(Ab)
+console.log(Cd)
+console.log(ef)
