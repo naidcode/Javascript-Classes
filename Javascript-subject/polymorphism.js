3b3ecd22218f62fd82dbc74bece4ed6a8443d43e
naidcode <naidcode.dev@gmail.com>
2025-09-07 03:29:18 +0530

    polymorphism-output-methods

--- Javascript-subject/polymorphism.js
+++ Javascript-subject/polymorphism.js
@@ -20,3 +20,30 @@ let second = new Student("Fazil pasha");
 
 first.title()
 second.title()
+
+class Animal {
+  speak() {
+    console.log("Some sound");
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log("Bark");
+  }
+}
+
+let  a = new Animal();    // first method to get ouput
+let b = new Dog();
+
+a.speak();
+b.speak()
+
+
+let animals = [new Animal() , new Dog()]    // second method
+for (const a of animals) {
+  a.speak()
+}
+
+let animals2 = [new Animal() , new Dog()]   //third method 
+animals2.forEach(a => a.speak());
