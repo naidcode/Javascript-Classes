1c1889b2974b49c3a34ccac648452f813622ebd2
naidcode <naidcode.dev@gmail.com>
2025-09-12 09:30:33 +0530

    formating

--- Javascript-subject/Promises.js
+++ Javascript-subject/Promises.js
@@ -2,43 +2,52 @@ function walkDog() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let waldoged = true;
-      if(waldoged){
-         resolve("let walk the dog")
-      } else{
-        reject("not done yet")
+
+      if (waldoged) {
+        resolve("let walk the dog");
+      } else {
+        reject("not done yet");
       }
-        }, 1500);
-    
-  })
+    }, 1500);
+  });
 }
 function clean() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      let cleaned = true;
-      if(cleaned){
-         resolve("done!")
-      } else{
-        reject("not done")
+      let cleaned = false;
+      if (cleaned) {
+        resolve("done!");
+      } else {
+        reject("not done");
       }
     }, 2500);
-    
-  })
+  });
 }
 
 function trash() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let trashed = false;
-      if(trashed){
-         resolve("done!")
-      } else{
-        reject("not done yet")
+      if (trashed) {
+        resolve("done!");
+      } else {
+        reject("not done yet");
       }
-    }, 500);  
-  })
+    }, 500);
+  });
 }
 
-walkDog().then(value => {console.log(value); return clean()})
-         .then(value => {console.log(value); return trash()})
-         .then(value => {console.log(value); return console.log("i finished my work")})
-         .catch(error => console.error(error))
+walkDog()
+  .then((value) => {
+    console.log(value);
+    return clean();
+  })
+  .then((value) => {
+    console.log(value);
+    return trash();
+  })
+  .then((value) => {
+    console.log(value);
+    return console.log("i finished my work");
+  })
+  .catch((error) => console.error(error))
