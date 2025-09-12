9a2c5973ca906e090e1103354b1960663ba7e33b
naidcode <naidcode.dev@gmail.com>
2025-09-12 15:18:49 +0530

    promise-promise.all-promise.race

--- Javascript-subject/Promises.js
+++ Javascript-subject/Promises.js
@@ -27,7 +27,7 @@ function clean() {
 function trash() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      let trashed = false;
+      let trashed = true;
       if (trashed) {
         resolve("done!");
       } else {
@@ -38,7 +38,7 @@ function trash() {
 }
 
 walkDog()
-  .then((value) => {
+  .then(value => {
     console.log(value);
     return clean();
   })
@@ -51,3 +51,44 @@ walkDog()
     return console.log("i finished my work");
   })
   .catch((error) => console.error(error))
+  .finally(() => console.log("always run the code "))
+
+
+function learn() {
+  return new Promise((resolve , reject) => {
+    setTimeout(() => {
+      let sum = 1+1;
+      if(sum <3){
+        resolve("correct")
+      } else{
+        reject("incorrect")
+      }
+    }, 5000);
+  })
+}
+ 
+
+  learn().then(value => console.log(`yes ${value}`))
+  .catch(error => console.log(`yes ${error}`))
+
+  function Task(name ,time) {
+    return new Promise((resolve , reject)=>{
+      setTimeout(() => {
+        resolve(`${name} completed`)
+      }, time);
+    })
+  }
+
+  Promise.all ([                                     // promise.all take all argument 
+    Task("nahid1" , 2000),
+    Task("nahid 2" , 1000),
+    Task("nahid 3" , 2500)
+  ]).then(value => console.log(value)).catch(console.log("nooo! "))
+
+
+    Promise.race ([                                     // promise.race only take which comes first
+    Task("nahid1" , 2000),
+    Task("nahid 2" , 1000),
+    Task("nahid 3" , 2500)
+  ]).then(value => console.log(value)).catch(console.log("nooo! "))
+  
