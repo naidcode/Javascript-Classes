3d4943334638ddd871be0371840e57f33c8ed3b2
naidcode <naidcode.dev@gmail.com>
2025-09-19 14:08:39 +0530

    day 12 exercises

--- Exercise-Javascript/day 12-exercise.js
+++ Exercise-Javascript/day 12-exercise.js
@@ -1,67 +1,121 @@
-// function fetchData() {
-//   return new Promise((resolve, reject) => {
-//     let success = true;
+// // function fetchData() {
+// //   return new Promise((resolve, reject) => {
+// //     let success = true;
 
-//     if (success) {
-//       resolve("Data loaded!");
-//     } else {
-//       reject("Something went wrong...");
+// //     if (success) {
+// //       resolve("Data loaded!");
+// //     } else {
+// //       reject("Something went wrong...");
+// //     }
+// //   });
+// // }
+
+// // fetchData()
+// //   .then(data => {
+// //     console.log(data);
+// //   })
+// //   .catch(error => {
+// //     console.log(error);
+// //   })
+// //   .finally(() => {
+// //     console.log("Process finished");
+// //   });
+
+
+//   class WeatherApp{
+//     #city
+//     constructor(city){
+//       this.#city = city
 //     }
-//   });
-// }
 
-// fetchData()
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   })
-//   .finally(() => {
-//     console.log("Process finished");
-//   });
+//     fetchWeather(){
+//       return new Promise((resolve , reject) => {
+//         setTimeout(() => {
+//           if(this.#city){
+//             resolve(`weather date for ${this.#city}: Sunny, 25°C`)
+//           } else{
+//             reject("city not found")
+//           }
+//         },2000 );
+//       })
+//     }
+
+//     showWeather(){
+//       this.fetchWeather().then(value => console.log(value)).catch(error => console.log(error))
+//     }
+
+//     handleError(){
+//       this.fetchWeather().catch(error => console.log(error))
+//     }
+
+//     processComplete(){
+//       this.fetchWeather().finally(() =>console.log("process completed successfully"))
+//     }
+//   }
 
 
-  class WeatherApp{
-    #city
-    constructor(city){
-      this.#city = city
-    }
+//   let weather = new WeatherApp("London")
 
-    fetchWeather(){
+//   weather.showWeather()
+//   weather.handleError()
+//   weather.processComplete()
+
+  
+// let weather2 = new WeatherApp(""); 
+// weather2.showWeather();       // Failure case
+// weather2.handleError();
+// weather2.processComplete()
+
+
+class FoodDelivery{
+  #OrderID;
+  constructor(orderID){
+    this.#OrderID = orderID
+  }
+
+  placeOrder(){
+    return new Promise((resolve , reject) => {
+      setTimeout(() => {
+        if(this.#OrderID){
+          resolve("Order placed: Pizza")
+        } else{
+          reject("no order yet")
+        }
+      }, 2000);
+    })
+  }
+
+    prepareOrder(){
       return new Promise((resolve , reject) => {
         setTimeout(() => {
-          if(this.#city){
-            resolve(`weather date for ${this.#city}: Sunny, 25°C`)
+          if(this.#OrderID){
+            resolve("Pizza Prepared")
           } else{
-            reject("city not found")
+            reject("we don't have any order")
           }
-        },2000 );
+          
+        }, 3000);
       })
     }
 
-    showWeather(){
-      this.fetchWeather().then(value => console.log(value)).catch(error => console.log(error))
+    deliverOrder(){
+      return new Promise((resolve , reject) => {
+        setTimeout(() => {
+          if(this.#OrderID){
+            resolve(`pizza Delivered`)
+          } else{
+            reject("no order ")
+          }
+          
+        }, 4000);
+      })
     }
+}
 
-    handleError(){
-      this.fetchWeather().catch(error => console.log(error))
-    }
+let foodDelivery = new FoodDelivery(1235689)
 
-    processComplete(){
-      this.fetchWeather().finally(() =>console.log("process completed successfully"))
-    }
-  }
-
-
-  let weather = new WeatherApp("London")
-
-  weather.showWeather()
-  weather.handleError()
-  weather.processComplete()
-
-  
-let weather2 = new WeatherApp(""); 
-weather2.showWeather();       // Failure case
-weather2.handleError();
-weather2.processComplete()
+foodDelivery.placeOrder().then(value => {console.log(value) ;return foodDelivery.prepareOrder();})
+                         .then(value => {console.log(value); return foodDelivery.deliverOrder();})
+                         .then(value => console.log(value))
+                         .catch(error => console.log(error))
+                         .finally(() => console.log("process Finished"))
