8e93da6e5d1c394188cc6c8c2d340904accfc6c5
naidcode <naidcode.dev@gmail.com>
2025-09-20 16:08:05 +0530

    promise

--- Javascript-subject/Promises.js
+++ Javascript-subject/Promises.js
@@ -58,6 +58,12 @@ walkDog()
       .then(result => console.log(result)); // final output: 12
    */
 
+
+      /* Async/Await = Async = makes a function return a promise
+          Await makes an async function wait for a promise
+          Allows you write asynchronous code in a synchronous manner
+          Async doesn't have resolve or reject parameters
+          Everything after Await is placed in an event queue */
   async function Done() {
     try{
 
