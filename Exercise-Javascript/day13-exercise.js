3e7a9d27e44677f6a7816d09c6813c0d6e387bd6
naidcode <naidcode.dev@gmail.com>
2025-09-21 21:52:39 +0530

    day 13 completed

--- Exercise-Javascript/day13-exercise.js
+++ Exercise-Javascript/day13-exercise.js
@@ -1,179 +1,209 @@
-class Book{
+class Book {
   #bookID
   #title
   #author
 
-  constructor(bookID , title , author){
-    this.#bookID = bookID,
-    this.#title = title,
+  constructor(bookID, title, author) {
+    this.#bookID = bookID
+    this.#title = title
     this.#author = author
   }
 
-  getID(){
+  getID() {
     return this.#bookID
   }
 
-  getTitle(){
+  getTitle() {
     return this.#title
   }
 
-  getAuthor(){
+  getAuthor() {
     return this.#author
   }
 }
 
-class User{
+class User {
   #Id
   #name
   #borrowedBooks = new Set()
 
-  constructor(Id , name){
-    this.#Id =  Id 
+  constructor(Id, name) {
+    this.#Id = Id
     this.#name = name
   }
 
-  getId(){
+  getId() {
     return this.#Id
   }
 
-  getName(){
+  getName() {
     return this.#name
   }
 
-  borrowBook(bookID){
-    if(this.#borrowedBooks.has(bookID)){
+  borrowBook(bookID) {
+    if (this.#borrowedBooks.has(bookID)) {
       console.log("book id already exist")
-    } else{
+    } else {
       this.#borrowedBooks.add(bookID)
       console.log(`${bookID} added successfully`)
     }
   }
 
-  returnBook(bookID){
-    if(this.#borrowedBooks.has(bookID)){
+  returnBook(bookID) {
+    if (this.#borrowedBooks.has(bookID)) {
       this.#borrowedBooks.delete(bookID)
       console.log(`${bookID} returned`)
-    } else{
+    } else {
       console.log("book id not found")
     }
   }
 
-  getBorrowedBooks(){
+  getBorrowedBooks() {
     return this.#borrowedBooks
   }
-
 }
 
-class Order{
+class Order {
   #orderId
   #userId
   #bookID
 
-  constructor(orderId , userId , bookID){
+  constructor(orderId, userId, bookID) {
     this.#orderId = orderId
     this.#userId = userId
     this.#bookID = bookID
   }
 
-  getOrderDetails(){
-    return this.#orderId
+  getOrderDetails() {
+    return {
+      orderId: this.#orderId,
+      userId: this.#userId,
+      bookId: this.#bookID
+    }
   }
 }
 
-class Library{
+class Library {
   #Books = new Map()
-  #users =  new Map()
+  #users = new Map()
   #orders = new Map()
 
-  addBook(book){
-    return new Promise((resolve , reject) => {
+  addBook(book) {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if(this.#Books.has(book.getID() , book)){
+        if (this.#Books.has(book.getID())) {
           reject("book already exist")
-        } else{
-          this.#Books.set(book)
+        } else {
+          this.#Books.set(book.getID(), book)
           resolve("book added successfully")
         }
-      }, 1000);
+      }, 1000)
     })
   }
 
-  registerUser(user){
-    return new Promise((resolve , reject) => {
+  registerUser(user) {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if(this.#users.has(user.getId() , user)){
+        if (this.#users.has(user.getId())) {
           reject("user already exist")
-        } else{
-          this.#users.set(user)
+        } else {
+          this.#users.set(user.getId(), user)
           resolve("user added successfully")
         }
-      }, 1300);
+      }, 1300)
     })
   }
 
-  placeOrder(order){
-    return new Promise((resolve , reject ) => {
+  placeOrder(order) {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if(this.#orders.has(order)){
+        if (this.#orders.has(order.getOrderDetails().orderId)) {
           reject("order already there")
-        } else{
-          this.#orders.set(order)
+        } else {
+          this.#orders.set(order.getOrderDetails().orderId, order)
           resolve("order added successfully")
         }
-      }, 1600);
+      }, 1600)
     })
   }
 
-  findBookById(bookID){
-    return new Promise((resolve , reject ) => {
+  findBookById(bookID) {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-              if(this.#Books.has(bookID)){
-        resolve(this.#Books.get(bookID));
-      } else {
-        reject("Book not found");
-      }
-
-      }, 1800);
-    })
-  }
-
-  findUserById(userId){
-    return new Promise((resolve , reject) => {
-      setTimeout(() => {
-                if(this.#users.has(userId)){
-          resolve(this.#users.get(userId));
+        if (this.#Books.has(bookID)) {
+          resolve(this.#Books.get(bookID))
         } else {
-          reject("user id not found");
+          reject("book not found")
         }
+      }, 1800)
+    })
+  }
 
-      }, 2000);
+  findUserById(userId) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (this.#users.has(userId)) {
+          resolve(this.#users.get(userId))
+        } else {
+          reject("user not found")
+        }
+      }, 2000)
     })
   }
 }
 
-async function fetchBookUserOrder(){
+// Example Usage
+const library = new Library()
+const book1 = new Book(123456, "JavaScript Mastery", "Nahid")
+const user1 = new User(990058, "Pasha")
+const order1 = new Order(456123, user1.getId(), book1.getID())
 
-   let results =  await Promise.all([findBookById(123456) , findUserById(990058) , placeOrder(456123)])
-  console.log(results)
-  } 
-
-  async function processOrders(){
-    let result2 =  await Promise.allSettled([findBookById(123456) , findUserById(990058) , placeOrder(456123)])
-    console.log(result2)
+async function fetchBookUserOrder() {
+  try {
+    let results = await Promise.all([
+      library.findBookById(123456),
+      library.findUserById(990058),
+      library.placeOrder(order1)
+    ])
+    console.log(results)
+  } catch (err) {
+    console.log("Error:", err)
   }
+}
 
-  async function raceCondition(){
-    let result3 = await Promise.race([findBookById(123456) , findUserById(990058) , placeOrder(456123)])
-    console.log(result3)
-  }
+async function processOrders() {
+  let result2 = await Promise.allSettled([
+    library.findBookById(123456),
+    library.findUserById(990058),
+    library.placeOrder(order1)
+  ])
+  console.log(result2)
+}
 
- async function anySuccess(){
-   let result4 =  await Promise.any([findBookById(123456) , findUserById(990058) , placeOrder(456123)])
-    console.log(result4)
-    
-  }
+async function raceCondition() {
+  let result3 = await Promise.race([
+    library.findBookById(123456),
+    library.findUserById(990058),
+    library.placeOrder(order1)
+  ])
+  console.log(result3)
+}
 
-  fetchBookUserOrder()
-  processOrders()
-  raceCondition()
-  anySuccess()
+async function anySuccess() {
+  let result4 = await Promise.any([
+    library.findBookById(123456),
+    library.findUserById(990058),
+    library.placeOrder(order1)
+  ])
+  console.log(result4)
+}
+
+library.addBook(book1)
+  .then(() => library.registerUser(user1))
+  .then(() => {
+    fetchBookUserOrder()
+    processOrders()
+    raceCondition()
+    anySuccess()
+  })
+  .catch(err => console.log(err))
