7f54366a9b16258f223c52b5e2f230cb504e295e
Nahid Pasha <naidcode.dev@gmail.com>
2025-10-20 13:57:41 +0530

    problems

--- Exercise-Javascript/problems.js
+++ Exercise-Javascript/problems.js
@@ -48,18 +48,32 @@ console.log(reverseString("hello"))
 function removeDuplicates(dupli){
    return  [...new Set(dupli)]
 }
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]))
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
-
-function findLongestWord(long){
-  let sep = long.split(' ')
-  let longs = sep[0]
-  for (const sepz of sep) {
-    if(sepz.length > longs.length){
-    longs = sepz
-    }
-  }
-  return longs
+// TODO: Rewrite these using .filter() and return the result
+function getEvenNumbers(){
+  return numbers.filter(n => n % 2 === 0)
 }
-console.log(findLongestWord("I love JavaScript programming"))
+
+let results = getEvenNumbers()
+console.log(results)
+
+function getGreaterThanFive(){
+  return numbers.filter(n => n >5)
+}
+let results2 = getGreaterThanFive()
+console.log(results2)
+
+function getLessThanOrEqualThree(){
+  return numbers.filter(n => n <=3)
+}
+let results3 = getLessThanOrEqualThree()
+console.log(results3)
+
+function flattenArray(arr){
+arr.forEach(arr1 => {
+  
+});
+}
+console.log(flattenArray([1, [2, 3], [4, [5, 6]]]));
+
